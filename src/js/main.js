90d213c708dbf0f9e3652a9a3a034fd677b92c0f
Brian Lundin <brian.lundin@gmail.com>
2018-07-24 18:08:33 -0500

    implemented reading progress bar at top of page

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+var h = document.documentElement,
+  b = document.body,
+  st = 'scrollTop',
+  sh = 'scrollHeight',
+  progress = document.querySelector('.reading-progress'),
+  scroll;
+
+if (progress != null) {
+  document.addEventListener('scroll', function() {
+    scroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+    progress.style.setProperty('--scroll', scroll + '%');
+  });
+}
