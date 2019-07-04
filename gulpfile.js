09a655f70d7c76979d1d31b06dc5978bb27eec30
Brian Lundin <brian.lundin@gmail.com>
2019-07-03 23:12:39 -0500

    updated packages + subscription page and popup for substack

--- gulpfile.js
+++ gulpfile.js
@@ -24,13 +24,13 @@ gulp.task('lint', function(done) {
 
 // Copy Javascript files from node_modules
 gulp.task('vendor-scripts', function(done) {
-  return gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.js',
+  return gulp.src(['./node_modules/js-cookie/src/js.cookie.js',
+                  './node_modules/bootstrap/dist/js/bootstrap.min.js',
                   './node_modules/jquery/dist/jquery.min.js',
                   './node_modules/popper.js/dist/umd/popper.min.js',
                   './node_modules/bootstrap/dist/js/bootstrap.min.js.map',
                   './node_modules/jquery/dist/jquery.min.map',
-                  './node_modules/popper.js/dist/umd/popper.min.js.map',
-                  './node_modules/js-cookie/src/js.cookie.js'])
+                  './node_modules/popper.js/dist/umd/popper.min.js.map'])
     .pipe(gulp.dest('./public/js/vendor'), done);
 });
 
